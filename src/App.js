3cac1c59f919a64c2cf64eb5275728cea2b6e1fd
Denis Tolic <denis.tolic28@gmail.com>
2023-05-15 20:05:40 +0200

    React Ispit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserDetails from './UserDetails';
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [userData, setUserData] = useState(null);
+
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchUserData();
+  };
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const data = await response.json();
+      setUserData(data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <input className="search" type="text" value={username} onChange={handleChange} placeholder="Enter GitHub username" />
+        <button className="submit" type="submit">Search</button>
+      </form>
+      {userData && <UserDetails userData={userData} />}
     </div>
   );
 }
